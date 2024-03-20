d704a993c72d62335098b37b165afdf315b43a80
itssyafrie <itssyafrie@gmail.com>
2024-03-20 16:08:05 +0700

    feat: add circle area calculator

---
+++ src/AreaOfCircle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class AreaOfCircle {
+    public static void main(String[] args){
+        System.out.println("Calculate The Area of A Circle Here!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the circle's radius in cm:");
+        double radius = scanner.nextDouble();
+        double area = Math.PI * Math.pow(radius, 2);
+        System.out.println("The area of the circle is: " + area + "cm");
+    }
+}
