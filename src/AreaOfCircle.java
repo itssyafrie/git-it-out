41ae1506d2b52ac0e0fa5f4b84e867dfae25adbb
itssyafrie <itssyafrie@gmail.com>
2024-03-20 16:20:44 +0700

    fix: change cm to cm square

--- src/AreaOfCircle.java
+++ src/AreaOfCircle.java
@@ -6,6 +6,6 @@ public class AreaOfCircle {
         System.out.println("Enter the circle's radius in cm:");
         double radius = scanner.nextDouble();
         double area = Math.PI * Math.pow(radius, 2);
-        System.out.println("The area of the circle is: " + area + "cm");
+        System.out.println("The area of the circle is: " + area + "cm square");
     }
 }
