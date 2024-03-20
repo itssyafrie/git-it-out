4dbbb7cbd2dce85fabc696c5858e12d21c701023
itssyafrie <itssyafrie@gmail.com>
2024-03-20 15:47:35 +0700

    fix: added greeting in the beginning of the program

--- src/AreaOfRectangle.java
+++ src/AreaOfRectangle.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class AreaOfRectangle {
     public static void main(String[] args){
+        System.out.println("Calculate The Area of A Rectangle Here!");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter rectangle length in cm:");
         double length = scanner.nextDouble();
